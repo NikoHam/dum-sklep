be273c8166a37c34d1b6363aa9e1b584fe25112f
NikoHam <58589799+NikoHam@users.noreply.github.com>
2022-04-01 14:58:19 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    switch (receivedNumber) {
+        case 10:
+            pins.servoWritePin(AnalogPin.P0, 0)
+            break;
+        case 11:
+            pins.servoWritePin(AnalogPin.P0, 180)
+            break;
 
+        case 20:
+            pins.servoWritePin(AnalogPin.P0, 0)
+            break;
+        case 21:
+            pins.servoWritePin(AnalogPin.P0, 180)
+            break;
+        case 30:
+            pins.servoWritePin(AnalogPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            break;
+        case 31:
+            pins.servoWritePin(AnalogPin.P0, 180)
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            break;
+    }
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+radio.setGroup(203)
